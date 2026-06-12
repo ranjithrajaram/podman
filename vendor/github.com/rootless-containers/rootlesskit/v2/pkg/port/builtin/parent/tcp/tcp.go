c252aa89218427a3de26b28fddd5be4b9a5245cf
Ranjith Rajaram <ranjith@redhat.com>
2026-06-12 22:23:21 +0530

    rootlessport: implement raw splice and poll event loop on EAGAIN
    Previously, rootlessport relied on Go's standard io.Copy() for proxying data
    between the host and the container. On Linux, this internally uses splice(2),
    which parks the goroutine on EAGAIN and yields back to Go's runtime scheduler.
    Under heavy load, this yields unbounded scheduler latency.
    
    This patch addresses this by implementing an epoll/poll-driven event loop that
    bypasses Go's scheduler entirely on EAGAIN.
    Specifically:
    - Uses runtime.LockOSThread() on the copying goroutines to prevent thread-hopping
      and scheduler interference.
    - Extracts the raw file descriptors from the net.Conn connections.
    - Sets both descriptors to non-blocking and manages splicing using a pipe.
    - Polls using unix.Poll on EAGAIN/EWOULDBLOCK directly without yielding to the
      scheduler.
    - Falls back to standard io.Copy in case raw file descriptors cannot be retrieved.
    
    Signed-off-by: Ranjith Rajaram <ranjith@redhat.com>

--- vendor/github.com/rootless-containers/rootlesskit/v2/pkg/port/builtin/parent/tcp/tcp.go
+++ vendor/github.com/rootless-containers/rootlesskit/v2/pkg/port/builtin/parent/tcp/tcp.go
@@ -5,11 +5,15 @@ import (
 	"io"
 	"net"
 	"os"
+	"runtime"
 	"strconv"
 	"sync"
+	"syscall"
 
 	"github.com/rootless-containers/rootlesskit/v2/pkg/port"
 	"github.com/rootless-containers/rootlesskit/v2/pkg/port/builtin/msg"
+	"github.com/sirupsen/logrus"
+	"golang.org/x/sys/unix"
 )
 
 func Run(socketPath string, spec port.Spec, stopCh <-chan struct{}, stoppedCh chan error, logWriter io.Writer) error {
@@ -74,24 +78,194 @@ func copyConnToChild(c net.Conn, socketPath string, spec port.Spec, stopCh <-cha
 	return nil
 }
 
+func getRawFd(conn net.Conn) (int, error) {
+	sconn, ok := conn.(interface {
+		SyscallConn() (syscall.RawConn, error)
+	})
+	if !ok {
+		return 0, fmt.Errorf("connection does not support SyscallConn")
+	}
+	rawConn, err := sconn.SyscallConn()
+	if err != nil {
+		return 0, err
+	}
+	var fdVal int
+	err = rawConn.Control(func(fd uintptr) {
+		fdVal = int(fd)
+	})
+	if err != nil {
+		return 0, err
+	}
+	return fdVal, nil
+}
+
+func waitForFd(fd int, events int16, quit <-chan struct{}) error {
+	pollFds := []unix.PollFd{
+		{Fd: int32(fd), Events: events},
+	}
+	for {
+		select {
+		case <-quit:
+			return fmt.Errorf("quit")
+		default:
+		}
+		n, err := unix.Poll(pollFds, 100)
+		if err != nil {
+			if err == unix.EINTR {
+				continue
+			}
+			return err
+		}
+		if n > 0 {
+			if pollFds[0].Revents&(unix.POLLERR|unix.POLLNVAL) != 0 {
+				return fmt.Errorf("fd error status")
+			}
+			if pollFds[0].Revents&events != 0 {
+				return nil
+			}
+			if pollFds[0].Revents&unix.POLLHUP != 0 {
+				return fmt.Errorf("hangup")
+			}
+		}
+	}
+}
+
+func spliceCopy(dstFd, srcFd int, quit <-chan struct{}) error {
+	var pipeFds [2]int
+	if err := unix.Pipe2(pipeFds[:], unix.O_CLOEXEC|unix.O_NONBLOCK); err != nil {
+		return err
+	}
+	pipeRead := pipeFds[0]
+	pipeWrite := pipeFds[1]
+	defer unix.Close(pipeRead)
+	defer unix.Close(pipeWrite)
+
+	runtime.LockOSThread()
+	defer runtime.UnlockOSThread()
+
+	if err := unix.SetNonblock(srcFd, true); err != nil {
+		return fmt.Errorf("setnonblock src: %w", err)
+	}
+	if err := unix.SetNonblock(dstFd, true); err != nil {
+		return fmt.Errorf("setnonblock dst: %w", err)
+	}
+
+	for {
+		select {
+		case <-quit:
+			return nil
+		default:
+		}
+
+		n, err := unix.Splice(srcFd, nil, pipeWrite, nil, 65536, unix.SPLICE_F_NONBLOCK|unix.SPLICE_F_MOVE)
+		if err != nil {
+			if err == unix.EAGAIN || err == unix.EWOULDBLOCK {
+				if err := waitForFd(srcFd, unix.POLLIN, quit); err != nil {
+					return err
+				}
+				continue
+			}
+			return err
+		}
+		if n == 0 {
+			return nil
+		}
+
+		inPipe := n
+		for inPipe > 0 {
+			select {
+			case <-quit:
+				return nil
+			default:
+			}
+
+			m, err := unix.Splice(pipeRead, nil, dstFd, nil, int(inPipe), unix.SPLICE_F_NONBLOCK|unix.SPLICE_F_MOVE)
+			if err != nil {
+				if err == unix.EAGAIN || err == unix.EWOULDBLOCK {
+					if err := waitForFd(dstFd, unix.POLLOUT, quit); err != nil {
+						return err
+					}
+					continue
+				}
+				return err
+			}
+			if m == 0 {
+				return io.ErrUnexpectedEOF
+			}
+			inPipe -= m
+		}
+	}
+}
+
 // bicopy is based on libnetwork/cmd/proxy/tcp_proxy.go .
 // NOTE: sendfile(2) cannot be used for sockets
 func bicopy(x, y net.Conn, quit <-chan struct{}) {
-	var wg sync.WaitGroup
-	var broker = func(to, from net.Conn) {
-		io.Copy(to, from)
-		if fromTCP, ok := from.(*net.TCPConn); ok {
-			fromTCP.CloseRead()
+	xFd, errX := getRawFd(x)
+	yFd, errY := getRawFd(y)
+
+	// Fallback to standard io.Copy if we cannot get raw fds
+	if errX != nil || errY != nil {
+		var wg sync.WaitGroup
+		var broker = func(to, from net.Conn) {
+			io.Copy(to, from)
+			if fromTCP, ok := from.(*net.TCPConn); ok {
+				fromTCP.CloseRead()
+			}
+			if toTCP, ok := to.(*net.TCPConn); ok {
+				toTCP.CloseWrite()
+			}
+			wg.Done()
 		}
-		if toTCP, ok := to.(*net.TCPConn); ok {
-			toTCP.CloseWrite()
+
+		wg.Add(2)
+		go broker(x, y)
+		go broker(y, x)
+		finish := make(chan struct{})
+		go func() {
+			wg.Wait()
+			close(finish)
+		}()
+
+		select {
+		case <-quit:
+		case <-finish:
 		}
-		wg.Done()
+		x.Close()
+		y.Close()
+		<-finish
+		return
 	}
 
+	// We use the raw splice event loop
+	var wg sync.WaitGroup
 	wg.Add(2)
-	go broker(x, y)
-	go broker(y, x)
+
+	go func() {
+		defer wg.Done()
+		if err := spliceCopy(yFd, xFd, quit); err != nil {
+			logrus.Debugf("spliceCopy x->y error: %v", err)
+		}
+		if yTCP, ok := y.(*net.TCPConn); ok {
+			yTCP.CloseWrite()
+		}
+		if xTCP, ok := x.(*net.TCPConn); ok {
+			xTCP.CloseRead()
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+		if err := spliceCopy(xFd, yFd, quit); err != nil {
+			logrus.Debugf("spliceCopy y->x error: %v", err)
+		}
+		if xTCP, ok := x.(*net.TCPConn); ok {
+			xTCP.CloseWrite()
+		}
+		if yTCP, ok := y.(*net.TCPConn); ok {
+			yTCP.CloseRead()
+		}
+	}()
+
 	finish := make(chan struct{})
 	go func() {
 		wg.Wait()
